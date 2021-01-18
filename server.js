575c71f667901ee763d520c24ea360d49c1c9cf3
Nawaf <n.alsuwailem@hotmail.com>
2021-01-18 20:19:14 +0300

    deployment commit

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require('express')
+const http = require('http');
 const app = express()
 const path = require('path');
 
 app.use(express.static(__dirname + '/dist'))
 
-app.listen(process.env.PORT || 8080)
+// app.listen(process.env.PORT || 8080)
+
+const port = process.env.PORT || 8080;
+
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 })
+
+app.all('*', function (req, res) {
+  res.status(200).sendFile(`/`, { root: dist });
+});
+
+const server = http.createServer(app);
+server.listen(port, () => console.log("Node Express server for " + app.name + " listening on port " + port));
 
 console.log('Console listening');
